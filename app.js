b4b03324a2ffe74ae01530d105c5a2a1ccfcc714
Abbat1 <cozzyyist@gmail.com>
2024-09-12 15:22:01 -0500

    traore_abba_project_capstone

---
+++ app.js
@@ -0,0 +1,9 @@
+/*document.addEventListener('DOMContentLoaded', () => {
+    const darkModeToggle = document.querySelector('.dark-mode-toggle');
+    const body = document.body;
+    
+    darkModeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+        darkModeToggle.textContent = body.classList.contains('dark-mode') ? '🌕' : '🌙';
+    });
+});*/
